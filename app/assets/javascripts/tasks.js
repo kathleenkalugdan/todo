77d87788e7e0493e8d58027345eb682e2ceb4480
kathleen kalugdan <kay16_alcantara@yahoo.com.ph>
2019-07-22 20:42:48 +0000

    polish deisgn

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -60,6 +60,8 @@
         var ulTodos = $('.todo-list');
               ulTodos.append(htmlString);
               $('.toggle').click(toggleTask);
+                    $('.new-todo').val('');
+
 
        });
     });
